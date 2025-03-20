aff06a8a57e6028d97c619135a4088d454b20398
Leonardo Larocca Vieira <5by5.vieira@voeazul.com.br>
2025-03-19 21:49:55 -0300

    update route name

--- src/Api/Controllers/FileController.cs
+++ src/Api/Controllers/FileController.cs
@@ -11,20 +11,14 @@ namespace Api.Controllers;
 public class FileController(ILogger<FileController> _logger, IMediator _mediator, IErrorProvider _errorProvider)
     : ApiControllerBase(_logger, _mediator, _errorProvider)
 {
-    [HttpPost("v1/file")]
-    public async Task<IActionResult> ConvertAsync([FromForm] IFormFile file) =>
-        await ProcessRequestAsync(new ConvertFileCommand(file), StatusCodes.Status200OK);
-
-
-    [HttpPost("v1/file/test")]
-    public async Task<IActionResult> Test([FromForm] IFormFile file)
+    [HttpPost("v1/file/convert")]
+    public async Task<IActionResult> ConvertAsync([FromForm] IFormFile file)
     {
         var response = await _mediator.Send(new ConvertFileCommand(file));
-
         return File(response.Value.Stream, "application/zip", "converted_files.zip");
     }
 
-    [HttpPost("v1/analyze-file")]
+    [HttpPost("v1/file/analyze")]
     public async Task<IActionResult> AnalyzeFileAsync(AnalyzeFileCommand request) =>
       await ProcessRequestAsync(request, StatusCodes.Status200OK);
 }
